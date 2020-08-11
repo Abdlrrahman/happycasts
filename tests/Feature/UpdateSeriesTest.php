99f57ce8f8caa8a6310baf08ae898a53c34f5154
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-08-11 16:30:07 +0200

    created the image not required to update a series test

--- tests/Feature/UpdateSeriesTest.php
+++ tests/Feature/UpdateSeriesTest.php
@@ -46,4 +46,34 @@ class UpdateSeriesTest extends TestCase
             'image_url' => 'series/new-series-title.png'
         ]);
     }
+
+    public function test_an_image_is_not_required_to_update_a_series()
+    {
+        $this->withoutExceptionHandling();
+
+        // login as admin
+        $this->loginAdmin();
+
+        //put request to the specified endpoint 
+        $series = factory(Series::class)->create();
+
+        Storage::fake(config('filesystems.default'));
+
+        $this->put(route('series.update', $series->slug), [
+            'title' => 'new series title',
+            'description' => 'new series description'
+        ])->assertRedirect(route('series.index'))
+            ->assertSessionHas('success', 'Successfully updated series');
+
+        //assert storage image
+        Storage::disk(config('filesystems.default'))->assertMissing(
+            'series/' . str_slug('new series title') . '.png'
+        );
+
+        //assert that the db has the series
+        $this->assertDatabaseHas('series', [
+            'slug' => str_slug('new series title'),
+            'image_url' => $series->image_url
+        ]);
+    }
 }
