254576e4df9783175ecf3f810e373274d8f43e86
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-08-13 12:20:50 +0200

    Added redis

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
+Route::get('/redis', function () {
+    //key: value //string
+    // Redis::set('friend', 'Alex');
+    // dd(Redis::get('friend'));
+
+    //key: value //list
+    // Redis::Lpush('farmeworks', ['vue.js', 'laravel']);
+    // dd(Redis::Lrange('farmeworks', 0, -1));
+
+    //key: value //set
+
+    // Redis::sadd('front-end-frameworks', ['vue.js', 'angular', 'react.js']);
+    // dd(Redis::smembers('front-end-frameworks'));
+});
+
 Route::get('/', 'FrontEndController@welcome');
 
 Route::get('/logout', function () {
