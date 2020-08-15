57f350975b474f4e78bd29ad95c2d3627c1b8834
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-08-15 14:08:49 +0200

    added the video id

--- resources/views/series.blade.php
+++ resources/views/series.blade.php
@@ -19,7 +19,7 @@
           @hasStartedSeries($series)
             <a href="{{ route('series.learning', $series->slug) }}" class="btn btn-lg btn-primary mr-16 btn-round">CONTINUE LEARNING</a>
           @else
-            <a class="btn btn-lg btn-primary mr-16 btn-round">START LEARNING</a>
+            <a href="{{ route('series.learning', $series->slug) }}" class="btn btn-lg btn-primary mr-16 btn-round">START LEARNING</a>
           @endhasStartedSeries
         @else
           <a class="btn btn-lg btn-primary mr-16 btn-round">START LEARNING</a>
