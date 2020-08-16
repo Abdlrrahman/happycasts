76de722d90f689bb6fc65ad50069bdffddca7513
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-08-16 11:47:52 +0200

    addedd the sweetalert

--- resources/assets/js/components/Player.vue
+++ resources/assets/js/components/Player.vue
@@ -6,17 +6,34 @@
 
 <script>
 import Player from "@vimeo/player";
+import Swal from "sweetalert";
+
 export default {
-  props: ["default_lesson"],
+  props: ["default_lesson", "next_lesson_url"],
+
+  methods: {
+    displayVideoEndedAlert() {
+      if (this.next_lesson_url) {
+        Swal("Congrats You completed this lesson !").then(() => {
+          window.location = this.next_lesson_url;
+        });
+      } else {
+        Swal("Congrats You completed this series !");
+      }
+    },
+  },
+
   data() {
     return {
       lesson: JSON.parse(this.default_lesson),
     };
   },
+
   mounted() {
     const player = new Player("handstick");
-    player.on("play", () => {
-      console.log("our video is playing !");
+
+    player.on("ended", () => {
+      this.displayVideoEndedAlert();
     });
   },
 };
