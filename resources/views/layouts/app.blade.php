976ba0fa1ede1d0b5038b9dc9e4e513be3d4fe23
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-08-17 14:57:27 +0200

    protected the admin routs and buttons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,10 +41,16 @@
           <ul class="topbar-nav nav">
             <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
             @auth
+@admin
               <li class="nav-item"><a href="{{ route('series.index') }}" class="nav-link">All
                   series</a></li>
               <li class="nav-item"><a href="{{ route('series.create') }}" class="nav-link">Create
                   series</a></li>
+
+                @else
+
+                @endadmin 
+
               <li class="nav-item">
                 <a class="nav-link" href="">{{ auth()->user()->name }}</a></li>
               </li>
@@ -91,39 +97,19 @@
       <div class="container">
         <div class="row gap-y align-items-center">
           <div class="col-12 col-lg-3">
-            <p class="text-center text-lg-left">
-              <a href="index.html"><img src='{{ asset('assets/img/logo.png') }}' alt="logo"></a>
-            </p>
           </div>
 
-          <div class="col-12 col-lg-6">
-            <ul class="nav nav-primary nav-hero">
-              <li class="nav-item">
-                <a class="nav-link" href="index.html">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="blog.html">Blog</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="block-feature.html">Features</a>
-              </li>
-              <li class="nav-item hidden-sm-down">
-                <a class="nav-link" href="page-pricing.html">Pricing</a>
-              </li>
-              <li class="nav-item hidden-sm-down">
-                <a class="nav-link" href="page-contact.html">Contact</a>
-              </li>
-            </ul>
+          <div class="col-12 col-lg-6 text-center">
+            <a class="nav-link" href="/"><h3>HappyCasts</h3></a> 
           </div>
 
           <div class="col-12 col-lg-3">
             <div class="social text-center text-lg-right">
-              <a class="social-facebook" href="https://www.facebook.com/thethemeio"><i class="fa fa-facebook"></i></a>
-              <a class="social-twitter" href="https://twitter.com/thethemeio"><i class="fa fa-twitter"></i></a>
-              <a class="social-instagram" href="https://www.instagram.com/thethemeio/"><i
-                  class="fa fa-instagram"></i></a>
-              <a class="social-dribbble" href="https://dribbble.com/thethemeio"><i class="fa fa-dribbble"></i></a>
-            </div>
+              <a class="social-facebook" href=""><i class="fa fa-facebook"></i></a>
+              <a class="social-twitter" href=""><i class="fa fa-twitter"></i></a>
+              <a class="social-instagram" href=""><i class="fa fa-instagram"></i></a>
+              <a class="social-github" href=""><i class="fa fa-github"></i></a>
+          </div>
           </div>
         </div>
       </div>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-// Get Routes
-
-Route::get('/redis', function () {
-    return view('series');
-});
-
 Auth::routes();
 
 Route::get('/', 'FrontEndController@welcome');
@@ -19,14 +13,14 @@ Route::get('/logout', function () {
     return redirect('/');
 });
 
-Route::get('/watch-series/{series}', 'WatchSeriesController@index')->name('series.learning');
-
-Route::get('/series/{series}/lesson/{lesson}', 'WatchSeriesController@showLesson')->name('series.watch');
-
 Route::get('/series/{series}', 'FrontendController@series')->name('series');
 
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile');
 
-// Post Routes
 
-Route::post("/series/complete-lesson/{lesson}", 'WatchSeriesController@completeLesson');
+
+Route::middleware('auth')->group(function () {
+    Route::post('/series/complete-lesson/{lesson}', 'WatchSeriesController@completeLesson');
+    Route::get('/watch-series/{series}', 'WatchSeriesController@index')->name('series.learning');
+    Route::get('/series/{series}/lesson/{lesson}', 'WatchSeriesController@showLesson')->name('series.watch');
+});
