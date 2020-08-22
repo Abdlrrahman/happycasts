cfd5e5b49efeee652434545f2d9f536e940ce922
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-08-22 15:01:21 +0200

    cleaned the web file and created the updateCard method

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,7 @@ Route::middleware('auth')->group(function () {
     Route::post('/series/complete-lesson/{lesson}', 'WatchSeriesController@completeLesson');
     Route::post('/subscribe', 'SubscriptionsController@subscribe');
     Route::post('/subscription/change', 'SubscriptionsController@change')->name('subscriptions.change');
-    Route::post('/card/update', function () {
-        $token = request('stripeToken');
-        $user = auth()->user();
-        $user->updateCard($token);
-        return response()->json('ok');
-    });
+    Route::post('/card/update', 'ProfilesController@updateCard');
     Route::get('/subscribe', 'SubscriptionsController@showSubscriptionForm');
     Route::get('/watch-series/{series}', 'WatchSeriesController@index')->name('series.learning');
     Route::get('/series/{series}/lesson/{lesson}', 'WatchSeriesController@showLesson')->name('series.watch');
