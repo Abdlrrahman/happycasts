ad807a65a630868939bf94c231936438193a1b93
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-08-23 23:25:56 +0200

    Deploying laravel to heroku is fun

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -19,5 +19,5 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB
+    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
 }
