41ad5520f8d30422a81b01fa4837d425b965c4e6
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-08-23 12:39:19 +0200

    documented the Learning trait

--- app/Entities/Learning.php
+++ app/Entities/Learning.php
@@ -8,17 +8,34 @@ use HappyCasts\Series;
 
 trait Learning
 {
-
+  /**
+   * Mark a lesson as completed for a user
+   *
+   * @param [HappyCasts\Lesson] $lesson
+   * @return void
+   */
   public function completeLesson($lesson)
   {
     Redis::sadd("user:{$this->id}:series:{$lesson->series->id}", $lesson->id);
   }
 
+  /**
+   * Get number of completed lessons for a series
+   *
+   * @param [HappyCasts\Series] $series
+   * @return void
+   */
   public function getNumberOfCompletedLessonsForASeries($series)
   {
     return count($this->getCompletedLessonsForASeries($series));
   }
 
+  /**
+   * Get percentage completed for a series for a user
+   *
+   * @param [HappyCasts\Series] $series
+   * @return void
+   */
   public function percentageCompletedForSeries($series)
   {
     $numberOfLessonsInSeries = $series->lessons->count();
@@ -27,16 +44,34 @@ trait Learning
     return ($numberOfCompletedLessons / $numberOfLessonsInSeries) * 100;
   }
 
+  /**
+   * Check if a user has started a series
+   *
+   * @param [HappyCasts\Series] $series
+   * @return boolean
+   */
   public function hasStartedSeries($series)
   {
     return $this->getNumberOfCompletedLessonsForASeries($series) > 0;
   }
 
+  /**
+   * Get array of completed lessons ids for a series
+   *
+   * @param [HappyCasts\Series] $series
+   * @return array
+   */
   public function getCompletedLessonsForASeries($series)
   {
     return Redis::smembers("user:{$this->id}:series:{$series->id}");
   }
 
+  /**
+   * Get all completed lessons for a series
+   *
+   * @param [HappyCasts\Series] $series
+   * @return \Illuminate\Support\Collection(HappyCasts\Lesson)
+   */
   public function getCompletedLessons($series)
   {
     $completedLessons = $this->getCompletedLessonsForASeries($series);
@@ -46,6 +81,12 @@ trait Learning
     )->get();
   }
 
+  /**
+   * Check if user has completed a lesson
+   *
+   * @param [HappyCasts\Lesson] $lesson
+   * @return boolean
+   */
   public function hasCompletedLesson($lesson)
   {
     return in_array(
@@ -54,6 +95,11 @@ trait Learning
     );
   }
 
+  /**
+   * Get all the series being watch ids
+   *
+   * @return array
+   */
   public function seriesBeingWatchedIds()
   {
     $keys = Redis::keys("user:{$this->id}:series:*");
@@ -66,6 +112,11 @@ trait Learning
     return $seriesIds;
   }
 
+  /**
+   * Get all the series a user is watching
+   *
+   * @return void
+   */
   public function seriesBeingWatched()
   {
     return collect($this->seriesBeingWatchedIds())->map(function ($id) {
@@ -73,6 +124,11 @@ trait Learning
     })->filter();
   }
 
+  /**
+   * Get total number of lessons user has ever completed
+   *
+   * @return integer
+   */
   public function getTotalNumberOfCompletedLessons()
   {
     $keys = Redis::keys("user:{$this->id}:series:*");
@@ -84,6 +140,12 @@ trait Learning
     return $result;
   }
 
+  /**
+   * Get the next lesson the user should watch
+   *
+   * @param [HappyCasts\Series] $series
+   * @return HappyCasts\Lesson
+   */
   public function getNextLessonToWatch($series)
   {
     $lessonIds = $this->getCompletedLessonsForASeries($series);
